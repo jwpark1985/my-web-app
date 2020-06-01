da74146447ac8c0379f0f744cab06fd3d62adf5c
jwpark1985 <jwpark1985@gmail.com>
2020-06-01 13:17:15 +0000

    Base template done

--- data/views.py
+++ data/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Document, Source
 
 # Create your views here.
 def document_list(request):
-    return render(request, 'data/document_list.html', {})
+    source = Source.objects.get(data_source='NC')
+    docs = Document.objects.filter(data_source=source)
+    return render(request, 'data/doc_list.html', {'docs': docs})
